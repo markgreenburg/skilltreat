00020a0a22e54f0ce01dadb65a95c167f1b68250
markgreenburg <marek.greenburg@gmail.com>
2017-03-16 13:07:43 -0500

    Implemented token revoke functionality

---
+++ models/token.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/**
+ * JWT Token storage and blacklisting
+ */
+
+module.exports = function(sequelize, DataTypes) {
+    const Token = sequelize.define('token', {
+        id: {
+            type: DataTypes.BIGINT,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        token: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            },
+        },
+        type: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+                isIn: [['verify', 'authenticate']],
+            },
+        },
+        revoked: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false,
+            validate: {
+                notEmpty: true,
+            },
+        },
+    });
+
+    return Token;
+}
