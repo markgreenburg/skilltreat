cefa59914f8670c763c8e30aab298a15afaf550e
markgreenburg <marek.greenburg@gmail.com>
2017-03-21 16:42:06 -0500

    Working on login form

--- SkilltreatNative/src/components/screens/Login.js
+++ SkilltreatNative/src/components/screens/Login.js
@@ -1,11 +1,33 @@
 'use strict';
 import React, { Component } from 'react';
-import { View, } from 'react-native'
-import { FormLabel, FormInput, } from 'react-native-elements';
+import { View, Text } from 'react-native'
+import { FormLabel, FormInput } from 'react-native-elements'
 
 class Login extends React.Component {
+    // constructor() {
+    //     this.submitLogin = this.submitLogin.bind(this);
+    // }
+    
+    // submitLogin() {
+    //     console.log("submitted!");
+    //     // Do some shit here to POST the login form
+    //     // Handle success, rejection
+    // }
+    
     render() {
-        return <Text>This is the login screen</Text>;
+        return (
+            <View>
+                <FormLabel>Email</FormLabel>
+                <FormInput/>
+                <FormLabel>Password</FormLabel>
+                <FormInput/>
+                {/*<Button
+                    title="Log In"
+                    onPress={submitLogin}
+                />*/}
+                <Text>Wee!</Text>
+            </View>
+        );
     }
 }
 
