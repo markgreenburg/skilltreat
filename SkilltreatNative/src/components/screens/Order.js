dcd95a4635083c7e3fd23c46e00f4e3cd66edfc5
markgreenburg <marek.greenburg@gmail.com>
2017-03-23 13:41:17 -0500

    Adding Orders to cart stack

---
+++ SkilltreatNative/src/components/screens/Order.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class Order extends React.Component {
+    render() {
+        return <Text>Create Order and Confirm</Text>
+    }
+}
+
+export default Order;
---
+++ SkilltreatNative/src/components/screens/OrderList.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class OrderList extends React.Component {
+    render() {
+        return <Text>Past Orders</Text>
+    }
+}
+
+export default OrderList;
