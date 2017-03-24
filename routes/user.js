37b600e06ba7400539823c32468d24bdd0328e71
markgreenburg <marek.greenburg@gmail.com>
2017-03-24 11:56:23 -0500

    Finished debug, removed debug code

--- routes/user.js
+++ routes/user.js
@@ -12,9 +12,6 @@ const auth = require('../middleware/authenticate');
 const Promise = require('bluebird');
 const uuid = require('uuid/v4');
 
-/* Debug */
-router.get('/user/debug', (req, res) => res.render('validated', {email: "someemail"}));
-
 /* Create a new user */
 router.post('/user/register', (req, res, next) => {
     // create new user
