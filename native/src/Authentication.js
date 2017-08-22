3f193be357df98a56186723100ed19fea7c7519f
markgreenburg <marek.greenburg@gmail.com>
2017-08-22 14:27:19 -0500

    cleaned up authenticator export

--- native/src/Authentication.js
+++ native/src/Authentication.js
@@ -4,8 +4,4 @@ const setToken = (token) => AsyncStorage.setItem('auth_token', token);
 const getToken = () => AsyncStorage.getItem('auth_token');
 const removeToken = () => AsyncStorage.removeItem('auth_token');
 
-module.exports = {
-    setToken: setToken,
-    getToken: getToken,
-    removeToken: removeToken,
-}
+module.exports = { setToken, getToken, removeToken, }
